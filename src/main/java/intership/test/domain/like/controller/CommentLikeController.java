8783f37eb4c904a3016cfc32c5296127eb0385ed
Seungcode <ehrehrdltkfk@gmail.com>
2024-06-27 23:38:38 +0900

    Fix : URL 수정
    기존 URL 중 하위 관계가 명확하지 않았던 부분 수정

--- src/main/java/intership/test/domain/like/controller/CommentLikeController.java
+++ src/main/java/intership/test/domain/like/controller/CommentLikeController.java
@@ -1,5 +1,6 @@
 package intership.test.domain.like.controller;
 
+import intership.test.domain.like.dto.CommentLikeCreate;
 import intership.test.domain.like.entity.LikeId;
 import intership.test.domain.like.service.CommentLikeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/board/{board_idx}/comment")
+@RequestMapping(value = "/board/comment")
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "Like API", description = "좋아요 관련 API")
@@ -19,11 +20,10 @@ public class CommentLikeController {
 
     @PostMapping("/{comment_idx}/like")
     public ResponseEntity<String> createCommentLike(
-            @PathVariable("board_idx") Long board_idx,
-            @PathVariable("comment_idx") Long comment_idx
+            @PathVariable("comment_idx") Long comment_idx,
+            @RequestBody Long user_id
             ){
-        log.info("Path Variable : {}", board_idx);
-        //commentLikeService.pressLike(likeId);
+        commentLikeService.pressLike(new CommentLikeCreate(user_id, comment_idx));
         return ResponseEntity.ok("좋아요 처리를 성공적으로 진행했습니다.");
     }
 }
--- src/main/java/intership/test/domain/like/service/CommentLikeService.java
+++ src/main/java/intership/test/domain/like/service/CommentLikeService.java
@@ -3,9 +3,13 @@ package intership.test.domain.like.service;
 import intership.test.domain.comment.entity.Comment;
 import intership.test.domain.comment.exception.CommentNotFound;
 import intership.test.domain.comment.repository.CommentRepository;
+import intership.test.domain.like.dto.CommentLikeCreate;
 import intership.test.domain.like.dto.CommentLikeMapper;
 import intership.test.domain.like.entity.CommentLike;
 import intership.test.domain.like.entity.LikeId;
+import intership.test.domain.user.dto.UserCreate;
+import intership.test.domain.user.entity.User;
+import intership.test.domain.user.exception.UserNotFound;
 import intership.test.domain.user.repository.UserRepository;
 import intership.test.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +22,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CommentLikeService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
 
     @Transactional
-    public void pressLike(LikeId likeId){
-        Comment comment = commentRepository.findById(likeId.getComment().getId()).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
-        CommentLike commentLike = CommentLikeMapper.toCommentLike(likeId);
+    public void pressLike(CommentLikeCreate commentLikeCreate){
+        User user = userRepository.findById(commentLikeCreate.getUser_id()).orElseThrow(() -> new UserNotFound(ErrorCode.USER_NOT_FOUND));
+        Comment comment = commentRepository.findById(commentLikeCreate.getComment_id()).orElseThrow(() -> new CommentNotFound(ErrorCode.COMMENT_NOT_FOUND));
+        CommentLike commentLike = CommentLikeMapper.toCommentLike(user, comment);
         Comment result = commentLike.pressLike(comment, commentLike);
         commentRepository.save(result);
     }
